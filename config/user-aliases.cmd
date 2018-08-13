73848a4305f4f3ff758854b574256c655cf9d22d
Archez <archez39@me.com>
2018-08-13 14:43:19 -0400

    Fix typo in alias

--- config/user-aliases.cmd
+++ config/user-aliases.cmd
@@ -16,9 +16,9 @@ vi=vim $*
 
 ccode=cd C:\Code
 emunexus=emulator -avd Nexus_5X_API_26 -no-audio $*
-githide=git update-index --assume-unchaged $*
+githide=git update-index --assume-unchanged $*
 gitf=git fetch $*
 gitl=git log --oneline --all --graph --decorate  $*
 gitmf=git merge --ff-only $*
 gits=git status $*
-gitunhide=git update-index --no-assume-unchaged $*
+gitunhide=git update-index --no-assume-unchanged $*
